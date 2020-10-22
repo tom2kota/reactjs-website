19ef276de4ccbad88c9895b4c0d13b9fad739d1e
Web Dev <tom2kota@gmail.com>
2020-10-22 03:14:57 +0300

    Initial commit

---
+++ src/pages/Preview.js
@@ -0,0 +1,24 @@
+import React, {useEffect} from 'react';
+import qs from 'qs';
+import {Loader} from '../components';
+import {linkResolver} from '../prismic-configuration';
+import {client} from '../utils/prismicHelpers';
+
+/**
+ * Prismic preview component
+ */
+const Preview = ({history, location}) => {
+    useEffect(() => {
+        const {token, documentId} = qs.parse(location.search.slice(1));
+        if (!token) {
+            return console.warn(`Unable to retrieve the session token from provided url. \n
+      Check https://prismic.io/docs/reactjs/beyond-the-api/in-website-preview for more info`)
+        }
+        client.getPreviewResolver(token, documentId).resolve(linkResolver, '/').then(url => history.push(url));
+    });
+
+
+    return <Loader/>;
+};
+
+export default Preview;
---
+++ src/prismic-configuration.js
@@ -0,0 +1,11 @@
+export const apiEndpoint = 'https://tom2kota.cdn.prismic.io/api/v2'
+// -- Access Token if the repository is not public
+// Generate a token in your dashboard and configure it here if your repository is private
+export const accessToken = ''
+
+// -- Link resolution rules
+// Manages the url links to internal Prismic documents
+export const linkResolver = (doc) => {
+    if (doc.type === 'page') return `/${doc.uid}`
+    return '/'
+}
